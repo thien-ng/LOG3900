f359127f7377df6d96386b7c938b3d4b148f1677
thien <d.thien.ng97@gmail.com>
2020-01-17 15:59:36 -0500

    add android application and server

---
+++ server/app/services/date.service.spec.ts
@@ -0,0 +1,32 @@
+import {expect} from 'chai';
+import * as sinon from 'sinon';
+import {DateService} from './date.service';
+
+
+describe('Date Service', () => {
+
+    let dateService: DateService;
+    let clock: sinon.SinonFakeTimers;
+   
+    beforeEach(()=>{
+        dateService = new DateService();
+        clock = sinon.useFakeTimers();
+    })
+
+    afterEach(()=>{
+        clock.restore();
+    })
+
+    it('currenTime should return a valid message', async () => {
+        let result = await dateService.currentTime();
+        expect(result.title).to.equals('Time');
+        expect(result.body).to.be.equal(new Date(0).toString());
+    });
+
+    it('currentTime should return different dates if called later', async () =>{
+        const { body: currentTime } = await dateService.currentTime();
+        clock.tick(5000);
+        const { body: now } = await dateService.currentTime();
+        expect(new Date(currentTime)).to.be.below(new Date(now));
+    })
+});
