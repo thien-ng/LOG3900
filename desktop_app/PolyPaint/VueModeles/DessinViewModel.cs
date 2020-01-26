0faf5007fe8b06ba5a0980f35d3f899995c2ee8d
Syn92 <Syn92@users.noreply.github.com>
2020-01-25 19:33:26 -0500

    ftr: ajout dessinVueModele

---
+++ desktop_app/PolyPaint/VueModeles/DessinViewModel.cs
@@ -0,0 +1,29 @@
+﻿using PolyPaint.Utilitaires;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PolyPaint.VueModeles
+{
+    class DessinViewModel : VueModele, IPageViewModel
+    {
+        private ICommand _goTo1;
+
+        public ICommand GoTo1
+        {
+            get
+            {
+                return _goTo1 ?? (_goTo1 = new RelayCommand(x =>
+                {
+                    Console.WriteLine("hello");
+                    Mediator.Notify("GoTo1Screen", "");
+                }));
+            }
+        }
+
+    }
+
+}
