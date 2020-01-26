2b7be534303f08a373ded9d35b6a5cb14e1c90b4
Syn92 <Syn92@users.noreply.github.com>
2020-01-25 19:26:22 -0500

    ftr: implementation userControl

--- desktop_app/PolyPaint/Vues/Login.xaml.cs
+++ desktop_app/PolyPaint/Vues/Login.xaml.cs
@@ -1,24 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
+using System.Windows.Controls;
+using PolyPaint.VueModeles;
 
 namespace PolyPaint.Vues
 {
     /// <summary>
     /// Interaction logic for Login.xaml
     /// </summary>
-    public partial class Login : Page
+    public partial class Login : UserControl
     {
         public Login()
         {
