10bdc47691cdb39fe47d52f0d590315ea7d9d0f2
Syn92 <Syn92@users.noreply.github.com>
2020-01-25 19:33:06 -0500

    ftr: ajout loginVueModele

---
+++ desktop_app/PolyPaint/VueModeles/LoginViewModel.cs
@@ -0,0 +1,27 @@
+﻿using PolyPaint.Utilitaires;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PolyPaint.VueModeles
+{
+    class LoginViewModel : VueModele, IPageViewModel
+    {
+        private ICommand _goTo2;
+
+        public ICommand GoTo2
+        {
+            get
+            {
+                return _goTo2 ?? (_goTo2 = new RelayCommand(x =>
+                {
+                    Mediator.Notify("GoTo2Screen", "");
+                }));
+            }
+        }
+
+    }
+}
