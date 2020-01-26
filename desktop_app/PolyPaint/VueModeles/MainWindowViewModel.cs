6c833784438fa91e05a9b1ad8d2aa623214c3cbc
Syn92 <Syn92@users.noreply.github.com>
2020-01-25 19:27:58 -0500

    ftr: ajout d'un mainWindowViewModel

---
+++ desktop_app/PolyPaint/VueModeles/MainWindowViewModel.cs
@@ -0,0 +1,71 @@
+﻿using PolyPaint.Utilitaires;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolyPaint.VueModeles
+{
+    class MainWindowViewModel : VueModele
+    {
+        private IPageViewModel _currentPageViewModel;
+        private List<IPageViewModel> _pageViewModels;
+
+        public List<Utilitaires.IPageViewModel> PageViewModels
+        {
+            get
+            {
+                if (_pageViewModels == null)
+                    _pageViewModels = new List<IPageViewModel>();
+
+                return _pageViewModels;
+            }
+        }
+
+        public IPageViewModel CurrentPageViewModel
+        {
+            get
+            {
+                return _currentPageViewModel;
+            }
+            set
+            {
+                _currentPageViewModel = value;
+                ProprieteModifiee("CurrentPageViewModel");
+            }
+        }
+
+        private void ChangeViewModel(IPageViewModel viewModel)
+        {
+            if (!PageViewModels.Contains(viewModel))
+                PageViewModels.Add(viewModel);
+
+            CurrentPageViewModel = PageViewModels
+                .FirstOrDefault(vm => vm == viewModel);
+        }
+
+        private void OnGo1Screen(object obj)
+        {
+            ChangeViewModel(PageViewModels[0]);
+        }
+
+        private void OnGo2Screen(object obj)
+        {
+            ChangeViewModel(PageViewModels[1]);
+        }
+
+        public MainWindowViewModel()
+        {
+            // Add available pages and set page
+            PageViewModels.Add(new LoginViewModel());
+            PageViewModels.Add(new DessinViewModel());
+
+            CurrentPageViewModel = PageViewModels[0];
+
+            Mediator.Subscribe("GoTo1Screen", OnGo1Screen);
+            Mediator.Subscribe("GoTo2Screen", OnGo2Screen);
+        }
+    }
+}
+
