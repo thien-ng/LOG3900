73470d83e24611772a0b4799d68ba21b13c67530
Jeremy Boulet <jeremyboulet85@gmail.com>
2020-01-28 12:16:27 -0500

    D: class user

--- desktop_app/PolyPaint/VueModeles/RegisterViewModel.cs
+++ desktop_app/PolyPaint/VueModeles/RegisterViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using PolyPaint.Utilitaires;
+using PolyPaint.Modeles;
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- desktop_app/PolyPaint/Vues/Register.xaml.cs
+++ desktop_app/PolyPaint/Vues/Register.xaml.cs
@@ -24,5 +24,10 @@ namespace PolyPaint.Vues
         {
             InitializeComponent();
         }
+
+        private void TextBlock_SourceUpdated(object sender, DataTransferEventArgs e)
+        {
+
+        }
     }
 }
