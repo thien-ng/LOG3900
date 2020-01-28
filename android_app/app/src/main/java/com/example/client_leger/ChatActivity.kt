f2711b10253cd41b835a3fdb47e2d0eff34e11ed
FX <francois-xavier.legault@polymtl.ca>
2020-01-28 12:35:48 -0500

    R: Using recycleView instead of listView for chat messages.

--- android_app/app/src/main/java/com/example/client_leger/ChatActivity.kt
+++ android_app/app/src/main/java/com/example/client_leger/ChatActivity.kt
@@ -2,10 +2,37 @@ package com.example.client_leger
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.fragment_chat.*
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.Item
+import com.xwray.groupie.ViewHolder
 
 class ChatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_chat)
+
+        val adapter = GroupAdapter<ViewHolder>()
+        recyclerView_chat_log.adapter = adapter
+    }
+}
+
+class ChatItemReceived: Item<ViewHolder>(){
+    override fun bind(viewHolder: ViewHolder, position: Int){
+
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.their_message_layout
+    }
+}
+
+class ChatItemSent: Item<ViewHolder>(){
+    override fun bind(viewHolder: ViewHolder, position: Int){
+
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.my_message_layout
     }
 }
