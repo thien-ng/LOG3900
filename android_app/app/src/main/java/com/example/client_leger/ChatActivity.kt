8493ebed406325179107343e7cd70074e56f4f04
FX <francois-xavier.legault@polymtl.ca>
2020-01-28 13:13:41 -0500

    D: Pressing enter now sends the message.

--- android_app/app/src/main/java/com/example/client_leger/ChatActivity.kt
+++ android_app/app/src/main/java/com/example/client_leger/ChatActivity.kt
@@ -2,6 +2,9 @@ package com.example.client_leger
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.KeyEvent
+import android.view.View
+import android.widget.EditText
 import kotlinx.android.synthetic.main.fragment_chat.*
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.Item
@@ -16,13 +19,26 @@ class ChatActivity : AppCompatActivity() {
 
         val adapter = GroupAdapter<ViewHolder>()
 
+        chat_message_editText.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
+                sendMessage(adapter, chat_message_editText)
+                return@OnKeyListener true
+            }
+            false
+        })
+
         chat_send_button.setOnClickListener {
-            //TODO: really send message.
-            adapter.add(ChatItemSent(chat_message_editText.text.toString()))
+            sendMessage(adapter, chat_message_editText)
         }
 
         recyclerView_chat_log.adapter = adapter
     }
+}
+
+fun sendMessage(adapter: GroupAdapter<ViewHolder>, textInput: EditText){
+    //TODO: really send message.
+    adapter.add(ChatItemSent(textInput.text.toString()))
+    textInput.text.clear()
 }
 
 class ChatItemReceived(val text: String): Item<ViewHolder>(){
