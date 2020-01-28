95752d855cf03c800012286d364668ffd0341827
FX <francois-xavier.legault@polymtl.ca>
2020-01-28 12:55:03 -0500

    D: Can now specify the text in the message to send/receive.

--- android_app/app/src/main/java/com/example/client_leger/ChatActivity.kt
+++ android_app/app/src/main/java/com/example/client_leger/ChatActivity.kt
@@ -6,6 +6,8 @@ import kotlinx.android.synthetic.main.fragment_chat.*
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.Item
 import com.xwray.groupie.ViewHolder
+import kotlinx.android.synthetic.main.my_message_layout.view.*
+import kotlinx.android.synthetic.main.their_message_layout.view.*
 
 class ChatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,13 +15,19 @@ class ChatActivity : AppCompatActivity() {
         setContentView(R.layout.fragment_chat)
 
         val adapter = GroupAdapter<ViewHolder>()
+
+        chat_send_button.setOnClickListener {
+            //TODO: really send message.
+            adapter.add(ChatItemSent(chat_message_editText.text.toString()))
+        }
+
         recyclerView_chat_log.adapter = adapter
     }
 }
 
-class ChatItemReceived: Item<ViewHolder>(){
+class ChatItemReceived(val text: String): Item<ViewHolder>(){
     override fun bind(viewHolder: ViewHolder, position: Int){
-
+        viewHolder.itemView.their_message_body.text = text
     }
 
     override fun getLayout(): Int {
@@ -27,9 +35,9 @@ class ChatItemReceived: Item<ViewHolder>(){
     }
 }
 
-class ChatItemSent: Item<ViewHolder>(){
+class ChatItemSent(val text: String): Item<ViewHolder>(){
     override fun bind(viewHolder: ViewHolder, position: Int){
-
+        viewHolder.itemView.my_message_body.text = text
     }
 
     override fun getLayout(): Int {
