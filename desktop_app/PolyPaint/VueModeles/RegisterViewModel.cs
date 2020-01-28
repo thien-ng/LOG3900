b1589f434a436e9c728e5fd4b486581d3ae06cf3
Syn92 <Syn92@users.noreply.github.com>
2020-01-28 14:03:48 -0500

    D: register fonctionnel

--- desktop_app/PolyPaint/VueModeles/RegisterViewModel.cs
+++ desktop_app/PolyPaint/VueModeles/RegisterViewModel.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Windows.Input;
 using System.Windows.Controls;
+using System.Collections.Generic;
+using System.Net.Http;
 
 namespace PolyPaint.VueModeles
 {
@@ -42,13 +44,30 @@ namespace PolyPaint.VueModeles
         {
             get
             {
-                return _register ?? (_register = new RelayCommand(x =>
+                return _register ?? (_register = new RelayCommand(async x =>
                 {
+                    await postRequestAsync(_username,Password.Password);
                     Console.WriteLine("User: " + this._username);
                     Console.WriteLine("pswd: " + this.Password.Password);
                     Console.WriteLine("cfrm: " + this.PasswordConfirm.Password);
                 }));
             }
+        }
+
+        public async System.Threading.Tasks.Task postRequestAsync(string username, string password)
+        {
+            var values = new Dictionary<string, string>
+                {
+                    { "username", username },
+                    { "password", password }
+                };
+
+            var content = new FormUrlEncodedContent(values);
+
+            var response = await VueModele.client.PostAsync("http://72.53.102.93:3000/account/register", content);
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(responseString);
         }
     }
 
