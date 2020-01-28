859c6525f28db6bc1c18f3a1101529eafd036165
FX <francois-xavier.legault@polymtl.ca>
2020-01-28 13:28:23 -0500

    D: Smooth scrolling on new messages.

--- android_app/app/src/main/java/com/example/client_leger/ChatActivity.kt
+++ android_app/app/src/main/java/com/example/client_leger/ChatActivity.kt
@@ -2,6 +2,7 @@ package com.example.client_leger
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.RecyclerView
 import android.view.KeyEvent
 import android.view.View
 import android.widget.EditText
@@ -20,26 +21,27 @@ class ChatActivity : AppCompatActivity() {
 
         val adapter = GroupAdapter<ViewHolder>()
 
-        chat_message_editText.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
+        chat_message_editText.setOnKeyListener(View.OnKeyListener { _, keyCode, event ->
             if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
-                sendMessage(adapter, chat_message_editText)
+                sendMessage(adapter, chat_message_editText, recyclerView_chat_log)
                 return@OnKeyListener true
             }
             false
         })
 
         chat_send_button.setOnClickListener {
-            sendMessage(adapter, chat_message_editText)
+            sendMessage(adapter, chat_message_editText, recyclerView_chat_log)
         }
 
         recyclerView_chat_log.adapter = adapter
     }
 }
 
-fun sendMessage(adapter: GroupAdapter<ViewHolder>, textInput: EditText){
+fun sendMessage(adapter: GroupAdapter<ViewHolder>, textInput: EditText, recyclerView: RecyclerView){
     //TODO: really send message.
     adapter.add(ChatItemSent(textInput.text.toString()))
     textInput.text.clear()
+    recyclerView.smoothScrollToPosition(adapter.itemCount)
 }
 
 class ChatItemReceived(val text: String): Item<ViewHolder>(){
