48b8f6def45e6d694a17eb19279ccc55a56b6e23
Duc-Thien Nguyen <duc-thien@sympatico.ca>
2020-02-09 00:19:14 -0500

    update travis

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,2 @@
 # Script to deploy server to Heroku
-git checkout Heroku_deployment
-git pull origin master
-
-rm -rf android_app
-rm -rf desktop_app
-rmdir desktop_app
-
-rm -f .travis.yml
-rm -f README.md
-rm -f deploy.sh
-
-
-git add .
-git commit -m "Deploy server to Heroku"
-git push
+rm yarn.lock
