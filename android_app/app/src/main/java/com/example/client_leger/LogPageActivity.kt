52f79eb234f443225e51fd5aec47b0be479a7aff
PChausseC <chaussepablo@gmail.com>
2020-02-13 16:33:29 -0500

    D: confirm password + error when fields are missing

--- android_app/app/src/main/java/com/example/client_leger/LogPageActivity.kt
+++ android_app/app/src/main/java/com/example/client_leger/LogPageActivity.kt
@@ -1,11 +1,13 @@
 package com.example.client_leger
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 
 import android.content.Intent
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_logpage.*
 import kotlinx.android.synthetic.main.fragment_login.*
@@ -21,14 +23,14 @@ class LogPageActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         login_button.isEnabled = true
         login_button.setOnClickListener {
-            if(login_editText_name.text.isNotBlank() && login_editText_password.text.isNotBlank()) {
+            if (login_editText_name.text.isNotBlank() && login_editText_password.text.isNotBlank()) {
                 login_button.isEnabled = false
                 var body = JSONObject()
                 body.accumulate("username", login_editText_name.text.toString().trim())
                 body.accumulate("password", login_editText_password.text.toString().trim())
                 controller.loginUser(this, this, body)
 
-            }else {
+            } else {
                 Toast.makeText(
                     applicationContext,
                     "Veuillez remplir tout les champs",
@@ -41,19 +43,37 @@ class LogPageActivity : AppCompatActivity() {
             setContentView(R.layout.fragment_registration)
             register_button.isEnabled = true
             register_button.setOnClickListener {
-                if(register_editText_name.text.isNotBlank() && register_editText_password.text.isNotBlank()) {
-                    register_button.isEnabled = false
-                    var body = JSONObject()
-                    body.accumulate("username", register_editText_name.text.toString().trim())
-                    body.accumulate("password", register_editText_password.text.toString().trim())
-                    controller.registerUser(this, this, body)
-                }else {
-                    Toast.makeText(
-                        applicationContext,
-                        "Veuillez remplir tout les champs",
-                        Toast.LENGTH_SHORT
-                    ).show()
+
+                when {
+                    register_editText_name.text.isBlank() -> {
+                        register_editText_name.error = "Enter a valid name"
+                        register_editText_name.requestFocus()
+                    }
+                    register_editText_password.text.isBlank() -> {
+                        register_editText_password.error = "Enter a valid password"
+                        register_editText_password.requestFocus()
+                    }
+                    register_editText_confirmPassword.text.isBlank() -> {
+                        register_editText_confirmPassword.error = "You need to confirm the password"
+                        register_editText_confirmPassword.requestFocus()
+                    }
+                    register_editText_confirmPassword.text.toString() != register_editText_password.text.toString() -> {
+                        register_editText_confirmPassword.error = "Password does not match"
+                        register_editText_confirmPassword.requestFocus()
+                    }
+                    else -> {
+                        closeKeyboard()
+                        register_button.isEnabled = false
+                        var body = JSONObject()
+                        body.accumulate("username", register_editText_name.text.toString().trim())
+                        body.accumulate(
+                            "password",
+                            register_editText_password.text.toString().trim()
+                        )
+                        controller.registerUser(this, this, body)
+                    }
                 }
+
             }
         }
     }
@@ -74,10 +94,15 @@ class LogPageActivity : AppCompatActivity() {
         }
     }
 
-    fun connect(username: String){
+    fun connect(username: String) {
         val intent = Intent(this, MainActivity::class.java)
         intent.putExtra("username", username)
         startActivity(intent)
     }
-    
+    private fun closeKeyboard(){
+        if ( this.currentFocus != null){
+            val imm: InputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(this.currentFocus.windowToken, 0)
+        }
+    }
 }
