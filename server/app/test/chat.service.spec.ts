358652e682eb9952e53b40199425daba02bb6150
Duc-Thien Nguyen <duc-thien@sympatico.ca>
2020-02-14 03:15:14 -0500

    D: add test for join/.leave channels

--- server/app/test/chat.service.spec.ts
+++ server/app/test/chat.service.spec.ts
@@ -51,6 +51,93 @@ describe("ChatService", () => {
         //then
         chai.expect(time.length).to.be.equal(8);
     });
-
 });
 
+describe("ChatService", () => {
+
+    let service: ChatService;
+
+    beforeEach(() => {
+        container.snapshot();
+        service = container.get<ChatService>(Types.ChatService);
+    });
+
+    afterEach(() => {
+        container.restore();
+    });
+
+    it("Should join channel succesfully", async () => {
+        // given
+        chai.spy.on(service["db"], "joinChannel", () => {});
+        chai.spy.on(service["db"], "getChannelsWithAccountName", () => {
+            return {rows:[{channel_id: "notEqualChannel"}]}
+        });
+
+        //when
+        const result = await service.joinChannel("username", "channel");
+
+        //then
+        chai.expect(result.status).to.be.equal(200);
+        chai.expect(result.message).to.be.equal("Successfully joined channel");
+    });
+
+    it("Should return already subscribe message when joining with existing channel", async () => {
+        // given
+        chai.spy.on(service["db"], "joinChannel", () => {});
+        chai.spy.on(service["db"], "getChannelsWithAccountName", () => {
+            return {rows:[{channel_id: "channel"}]}
+        });
+
+        //when
+        const result = await service.joinChannel("username", "channel");
+
+        //then
+        chai.expect(result.status).to.be.equal(400);
+        chai.expect(result.message).to.be.equal("username is already subscribed to channel.");
+    });
+
+    it("Should leave channel successfully", async () => {
+        // given
+        chai.spy.on(service["db"], "leaveChannel", () => {});
+        chai.spy.on(service["db"], "getChannelsWithAccountName", () => {
+            return {rows:[{channel_id: "channel"}]}
+        });
+
+        //when
+        const result = await service.leaveChannel("username", "channel");
+
+        //then
+        chai.expect(result.status).to.be.equal(200);
+        chai.expect(result.message).to.be.equal("Successfully left channel");
+    });
+
+    it("Should return is not subscribed to channel when user is not sub to channel", async () => {
+        // given
+        chai.spy.on(service["db"], "leaveChannel", () => {});
+        chai.spy.on(service["db"], "getChannelsWithAccountName", () => {
+            return {rows:[{channel_id: "notChannel"}]}
+        });
+
+        //when
+        const result = await service.leaveChannel("username", "channel");
+
+        //then
+        chai.expect(result.status).to.be.equal(400);
+        chai.expect(result.message).to.be.equal("username is not subscribed to channel.");
+    });
+
+    it("Should return cannot leave default channel when leaving general", async () => {
+        // given
+        chai.spy.on(service["db"], "leaveChannel", () => {});
+        chai.spy.on(service["db"], "getChannelsWithAccountName", () => {
+            return {rows:[{channel_id: "general"}]}
+        });
+
+        //when
+        const result = await service.leaveChannel("username", "general");
+
+        //then
+        chai.expect(result.status).to.be.equal(400);
+        chai.expect(result.message).to.be.equal("cannot leave default channel: general.");
+    });
+});
