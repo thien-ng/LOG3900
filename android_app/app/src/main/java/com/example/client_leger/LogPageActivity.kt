b44af5aa1564dd0795988435d8c1fbe3609c98ce
Duc-Thien Nguyen <duc-thien@sympatico.ca>
2020-02-22 04:02:32 -0500

    D: add bottom navigation view

--- android_app/app/src/main/java/com/example/client_leger/LogPageActivity.kt
+++ android_app/app/src/main/java/com/example/client_leger/LogPageActivity.kt
@@ -3,7 +3,6 @@ package com.example.client_leger
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.example.client_leger.Fragments.LoginFragment
-import kotlinx.android.synthetic.main.activity_logpage.*
 
 
 class LogPageActivity : AppCompatActivity() {
@@ -11,7 +10,6 @@ class LogPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_logpage)
-        setSupportActionBar(toolbar)
 
         supportFragmentManager.beginTransaction().replace(R.id.container_view, LoginFragment()).commit()
     }
--- android_app/app/src/main/java/com/example/client_leger/MainActivity.kt
+++ android_app/app/src/main/java/com/example/client_leger/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.client_leger
 
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import com.example.client_leger.Fragments.ChatFragment
+import com.example.client_leger.Fragments.DrawFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,11 +13,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportFragmentManager.beginTransaction().replace(R.id.container_view, ChatFragment()).commit()
+        bottomNavigationView.setOnNavigationItemSelectedListener(onNavigationListener)
+        bottomNavigationView.selectedItemId = R.id.action_chat
     }
 
     override fun onBackPressed() {
         // To ignore back button pressed.
+    }
+
+    private val onNavigationListener = BottomNavigationView.OnNavigationItemSelectedListener  {menuItem ->
+        when (menuItem.itemId) {
+            R.id.action_chat -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.container_view, ChatFragment()).commit()
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.action_profil -> {
+                // TODO
+                return@OnNavigationItemSelectedListener false
+            }
+            R.id.action_game -> {
+                setContentView(DrawFragment(this, null))
+                return@OnNavigationItemSelectedListener false
+            }
+        }
+        return@OnNavigationItemSelectedListener false
     }
 
 }
