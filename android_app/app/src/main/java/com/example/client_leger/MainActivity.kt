2afff6da80cba61466218459fb2d44e1ffdeb214
Duc-Thien Nguyen <duc-thien@sympatico.ca>
2020-02-23 23:48:55 -0500

    D: fix login for mobl client

--- android_app/app/src/main/java/com/example/client_leger/MainActivity.kt
+++ android_app/app/src/main/java/com/example/client_leger/MainActivity.kt
@@ -14,6 +14,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        SocketIO.connect(this!!.intent.getStringExtra("username"))
+
         bottomNavigationView.setOnNavigationItemSelectedListener(onNavigationListener)
         bottomNavigationView.selectedItemId = R.id.action_chat
     }
