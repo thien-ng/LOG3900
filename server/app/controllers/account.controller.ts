c83e9f868b6553a7d6ac43c4b45104a72c632dd7
thien <d.thien.ng97@gmail.com>
2020-02-26 13:30:05 -0500

    D: add swagger configs

--- server/app/controllers/account.controller.ts
+++ server/app/controllers/account.controller.ts
@@ -17,14 +17,41 @@ export class AccountController {
     private configureRouter() {
         this.router = Router();
 
+        /**
+         * @swagger
+         * /account/register:
+         *  post:
+         *    description: Use to register a new user
+         *    responses:
+         *      '200':
+         *          description: A successful response
+         */
         this.router.post('/register', async (req: Request, res: Response, next: NextFunction) => {
             res.json(await this.accountService.register(req.body));
         });
 
+        /**
+         * @swagger
+         * /account/login:
+         *  post:
+         *    description: Use to log a user
+         *    responses:
+         *      '200':
+         *          description: A successful response
+         */
         this.router.post('/login', async (req: Request, res: Response, next: NextFunction) => {
             res.json(await this.accountService.login(req.body));
         });
 
+        /**
+         * @swagger
+         * /account/users/online:
+         *  get:
+         *    description: Use to get online users
+         *    responses:
+         *      '200':
+         *          description: A successful response
+         */
         this.router.get('/users/online', (req: Request, res: Response, next: NextFunction) => {
             res.json(this.userService.getOnlineUsers());
         });
