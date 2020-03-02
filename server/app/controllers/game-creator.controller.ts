a2f25be6aaa491704acb41dfa6a4b598e7d28eb3
Duc-Thien Nguyen <duc-thien@sympatico.ca>
2020-03-02 01:58:55 -0500

    D: add controller for game creator

--- server/app/controllers/game-creator.controller.ts
+++ server/app/controllers/game-creator.controller.ts
@@ -1,20 +1,21 @@
 import { NextFunction, Request, Response, Router } from 'express';
 import { inject, injectable } from 'inversify';
+import { GameCreatorService } from '../services/game/game-creator.service';
 import Types from '../types';
 
 @injectable()
 export class GameCreatorController {
     public router: Router;
 
-    public constructor() {
+    public constructor(@inject(Types.GameCreatorService) private creatorServ: GameCreatorService) {
         this.configureRouter();
     }
 
     private configureRouter() {
         this.router = Router();
 
-        this.router.post('/game', (req: Request, res: Response, next: NextFunction) => {
-            res.json();
+        this.router.get('/game/suggestion', async (req: Request, res: Response, next: NextFunction) => {
+            res.json(await this.creatorServ.getSuggestion());
         });
     }
 }
--- server/app/services/game/game-creator.service.ts
+++ server/app/services/game/game-creator.service.ts
@@ -1,5 +1,15 @@
 import { injectable } from "inversify";
 
+const quickDraw = require("quickdraw.js");
+
+@injectable()
 export class GameCreatorService {
+
+    public async getSuggestion() {
+        await quickDraw.import('broccoli', 1);
+        // console.log("value :", quickDraw.set(1 ,["broccoli"]));
+        
+        return quickDraw.set(1 ,["broccoli"]).set[0].input;
+    }
 
 }
