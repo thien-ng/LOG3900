77f22f1ab6efa12099a9a87968fc5136f4f568ea
Jacob Dorais <jacobdorais25@hotmail.com>
2020-03-12 15:16:27 -0400

    F: ajout du hint au joueur virtuel

--- server/app/bot.ts
+++ server/app/bot.ts
@@ -9,33 +9,42 @@ export class Bot {
     private username: string;
     private image: IDrawing[];
     private nextStroke: number;
+    private hint: string;
 
-    constructor(username: string, image: IDrawing[], @inject(Types.GameManagerService) private gameManager: GameManagerService) {
+    constructor(image: IDrawing[],
+        username: string = "BOT:bob",
+        hint: string = "no hint for you!",
+        @inject(Types.GameManagerService) private gameManager: GameManagerService) {
 
         this.username = username;
         this.image = image;
+        this.hint = hint;
         this.findStroke();
     }
 
-    public sendNextStroke(): void {
+    public sendNextStroke(): boolean {
         const out: any = this.image[this.nextStroke];           // whacky stuff here
         out.username = this.username;                           // whacky stuff here
         const res: IGameplayDraw = out;                         // whacky stuff here
         this.gameManager.sendMessageToArena(res);               // ceci ou la fonction qui enverra a tout le monde
         this.findStroke();
+        return !this.isDone();   // retourne true si il reste des traits a ajouter.
     }
 
-    private findStroke(): void {
+    private isDone(): boolean {
+        return (this.nextStroke == this.image.length);
+    }
+
+    private findStroke(): void { // quand le dernier trait a ete envoyee, on met le prochain a image.length;
         if (this.nextStroke == null) {
             this.nextStroke = 0;
             return;
         }
-
         this.nextStroke++;
     }
 
-    public d(): void {
-
+    public getHint(): string {
+        return this.hint;
     }
 
 }
