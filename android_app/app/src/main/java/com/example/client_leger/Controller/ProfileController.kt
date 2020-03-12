9832cb816dbc8276961696b03147e59c86219137
PChausseC <chaussepablo@gmail.com>
2020-03-12 17:16:19 -0400

    D: layouts for connection and disconnection item in  in user history list

--- android_app/app/src/main/java/com/example/client_leger/Controller/ProfileController.kt
+++ android_app/app/src/main/java/com/example/client_leger/Controller/ProfileController.kt
@@ -3,14 +3,11 @@ package com.example.client_leger.Controller
 import android.widget.Toast
 import com.android.volley.Request
 import com.android.volley.Response
-import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.example.client_leger.Constants
 import com.example.client_leger.Fragments.ProfilFragment
 import com.example.client_leger.models.User
-import org.json.JSONArray
-import org.json.JSONObject
 
 class ProfileController {
     fun getUserProfile(fragment: ProfilFragment, username:String){
@@ -20,8 +17,18 @@ class ProfileController {
             Request.Method.GET,
             Constants.SERVER_URL + Constants.USER_INFO_ENDPOINT + username,
             null,
-            Response.Listener<JSONObject>{ response ->
+            Response.Listener { response ->
                 fragment.binding.user = User(username, response.getString("firstName"), response.getString("lastName"), response.getJSONArray("connections"))
+                (0 until fragment.binding!!.user!!.connections!!.length()).forEach { i ->
+                    var item = fragment.binding!!.user!!.connections.getJSONObject(i)
+                    if(item.getBoolean("isLogin")) {
+                        fragment.adapter.addLoggedIn(item.getString("times"))
+                    } else {
+                        fragment.adapter.addLoggedOut(item.getString("times"))
+                    }
+
+                    fragment.adapter.getItemViewType(i)
+                }
             }, Response.ErrorListener{
                     error ->
                 Toast.makeText(
--- android_app/app/src/main/java/com/example/client_leger/Fragments/ProfilFragment.kt
+++ android_app/app/src/main/java/com/example/client_leger/Fragments/ProfilFragment.kt
@@ -6,17 +6,22 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
+import com.example.client_leger.Adapters.ConnexionListViewAdapter
 import com.example.client_leger.Controller.ProfileController
 import com.example.client_leger.R
 import com.example.client_leger.databinding.FragmentProfilBinding
-import com.example.client_leger.models.User
 
 class ProfilFragment: Fragment() {
     lateinit var binding: FragmentProfilBinding
     private var profileController:ProfileController = ProfileController()
+    lateinit var  adapter: ConnexionListViewAdapter
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate<FragmentProfilBinding>(inflater,R.layout.fragment_profil, container, false)
+        adapter = ConnexionListViewAdapter(this.context)
         profileController.getUserProfile(this, activity!!.intent.getStringExtra("username"))
+        var listview = binding.root.findViewById<ListView>(R.id.listView);
+        listview.adapter = adapter
         return binding.root
     }
 }
