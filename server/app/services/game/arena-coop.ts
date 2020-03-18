17f808fa41fa77e7d3158e64029d34eb4b201256
Duc-Thien Nguyen <duc-thien@sympatico.ca>
2020-03-18 04:38:00 -0400

    D: add fucntion to make it compile

--- server/app/services/game/arena-coop.ts
+++ server/app/services/game/arena-coop.ts
@@ -1,8 +1,14 @@
 import { Arena } from "./arena";
+import { IGameplayChat, IGameplayDraw } from "../../interfaces/game";
 
+import * as io from 'socket.io';
 
 export class ArenaCoop extends Arena {
 
     // TODO implement coop
+
+    public start(): void {}
+
+    public receiveInfo(socket: io.Socket, mes: IGameplayChat | IGameplayDraw): void {}
 
 }
--- server/app/services/game/arena-solo.ts
+++ server/app/services/game/arena-solo.ts
@@ -1,8 +1,13 @@
 import { Arena } from "./arena";
+import { IGameplayChat, IGameplayDraw } from "../../interfaces/game";
 
+import * as io from 'socket.io';
 
 export class ArenaSolo extends Arena {
 
     // TODO implement solo
 
+    public start(): void {}
+
+    public receiveInfo(socket: io.Socket, mes: IGameplayChat | IGameplayDraw): void {}
 }
