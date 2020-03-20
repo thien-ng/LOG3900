0a669105bd04d0fa433ee44a99000d05336d751d
PChausseC <chaussepablo@gmail.com>
2020-03-20 19:21:59 -0400

    D: getusers

--- android_app/app/src/main/java/com/example/client_leger/Controller/GameController.kt
+++ android_app/app/src/main/java/com/example/client_leger/Controller/GameController.kt
@@ -3,6 +3,7 @@ package com.example.client_leger.Controller
 import android.widget.Toast
 import com.android.volley.Request
 import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.example.client_leger.Constants
@@ -31,5 +32,28 @@ class GameController {
         )
 
         requestQueue.add(jsonObjectRequest)
+    }
+
+    fun getUsers(fragment: LobbyFragment, lobbyName: String) {
+        val requestQueue = Volley.newRequestQueue(fragment.context)
+
+        val jsonArrayRequest = JsonArrayRequest(
+            Request.Method.GET,
+            Constants.SERVER_URL + Constants.USERS_LOBBY_ENDPOINT + lobbyName,
+            null,
+            Response.Listener {response ->
+                fragment.loadUsers(response)
+            }, Response.ErrorListener{
+                    error ->
+                Toast.makeText(
+                    fragment.context,
+                    error.message,
+                    Toast.LENGTH_SHORT
+                ).show()
+
+            }
+        )
+
+        requestQueue.add(jsonArrayRequest)
     }
 }
