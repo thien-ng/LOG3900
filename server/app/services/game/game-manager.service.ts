4ea196a4975abda82cea0d19f1803ab905be3bef
Jacob Dorais <jacobdorais25@hotmail.com>
2020-03-26 14:58:18 -0400

    F: ajout de la verification

--- server/app/services/game/game-manager.service.ts
+++ server/app/services/game/game-manager.service.ts
@@ -20,19 +20,19 @@ export class GameManagerService {
     private arenaId: number;
 
     private socketServer: io.Server;
-    
+
     public constructor(
-        @inject(Types.LobbyManagerService)  private lobServ: LobbyManagerService,
-        @inject(Types.RulesDbService)       private rulesDb: RulesDbService,
-        @inject(Types.GameDbService)        private gameDb: GameDbService) {
+        @inject(Types.LobbyManagerService) private lobServ: LobbyManagerService,
+        @inject(Types.RulesDbService) private rulesDb: RulesDbService,
+        @inject(Types.GameDbService) private gameDb: GameDbService) {
 
         this.arenas = new Map<number, ArenaFfa | ArenaSolo | ArenaCoop>();
         this.userMapArenaId = new Map<string, number>();
         this.arenaId = 0;
     }
-    
+
     public initSocketServer(socketServer: io.Server): void {
-        this.socketServer = socketServer;     
+        this.socketServer = socketServer;
     }
 
     public startGame(lobbyName: string): void {
@@ -40,8 +40,30 @@ export class GameManagerService {
 
         if (!lobby)
             throw new Error(`${lobbyName} is not found in active lobbies`);
+        this.verifyAmountPlayer(lobby);
 
         this.setupArena(lobby);
+    }
+
+    private verifyAmountPlayer(lobby: IActiveLobby): void {
+        let minAmount: number = 0, maxAmount: number = 0;
+        switch (lobby.mode) {
+            case GameMode.FFA:
+                minAmount = 2;
+                maxAmount = 12;
+                break;
+            case GameMode.COOP:
+                minAmount = 2;
+                maxAmount = 4;
+                break;
+            case GameMode.SOLO:
+                minAmount = 1;
+                maxAmount = 1;
+                break;
+        }
+        if (lobby.users.length < minAmount || lobby.users.length > maxAmount) {
+            throw new Error(`the amount of players in the lobby must be between ${minAmount} and ${maxAmount} to play in ${lobby.mode}.`);
+        }
     }
 
     public sendMessageToArena(socket: io.Socket, mes: IGameplayChat | IGameplayDraw | IGameplayReady): void {
@@ -62,14 +84,14 @@ export class GameManagerService {
         this.addUsersToArena(lobby, room, this.arenaId);
 
         const rules: IGameRule[] = await this.rulesDb.getRules();
-        
+
         if (rules.length < lobby.users.length)
             throw new Error("Not enough drawings are in db");
 
         const arena = this.createArenaAccordingToMode(this.arenaId, lobby, room, rules);
 
         this.lobServ.lobbies.delete(lobby.lobbyName);
-        
+
         this.arenas.set(this.arenaId, arena);
         this.arenaId++;
 
@@ -79,7 +101,7 @@ export class GameManagerService {
     }
 
     private createArenaAccordingToMode(arenaId: number, lobby: IActiveLobby, room: string, rules: IGameRule[]): ArenaFfa | ArenaSolo | ArenaCoop {
-        switch(lobby.mode) {
+        switch (lobby.mode) {
             case GameMode.FFA:
                 return new ArenaFfa(GameMode.FFA, arenaId, lobby.users, room, this.socketServer, rules, this);
             case GameMode.SOLO:
@@ -101,7 +123,7 @@ export class GameManagerService {
         const winner = this.determineWinner(pts);
         const users = this.getPlayersInGame(pts);
         const date = Time.today();
-        this.gameDb.registerGame({type: type, date: date, timer: timer, winner: winner, users: users});
+        this.gameDb.registerGame({ type: type, date: date, timer: timer, winner: winner, users: users });
     }
 
     private determineWinner(pts: IPoints[]): string {
@@ -113,17 +135,17 @@ export class GameManagerService {
                 maxPts = p.points;
             }
         });
-        
+
         return winner;
     }
 
     private getPlayersInGame(pts: IPoints[]): IUserPt[] {
         const list: IUserPt[] = [];
-        pts.forEach(p => {list.push({username: p.username, point: p.points})});
+        pts.forEach(p => { list.push({ username: p.username, point: p.points }) });
         return list;
     }
 
-    private addUsersToArena(lobby: IActiveLobby, room: string, arenaID: number): void {        
+    private addUsersToArena(lobby: IActiveLobby, room: string, arenaID: number): void {
         // add users to socket room
         lobby.users.forEach(u => {
             if (u.socket) {
@@ -132,5 +154,5 @@ export class GameManagerService {
             }
         });
     }
-    
+
 }
