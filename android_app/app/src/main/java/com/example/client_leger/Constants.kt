4e7aa88be5fe38ae88b66f27a6e24e2a22b087d5
PChausseC <chaussepablo@gmail.com>
2020-03-27 15:15:46 -0400

    B: leave lobby endpoint

--- android_app/app/src/main/java/com/example/client_leger/Constants.kt
+++ android_app/app/src/main/java/com/example/client_leger/Constants.kt
@@ -15,7 +15,7 @@ class Constants {
         const val USER_INFO_ENDPOINT = "/account/user/info/"
         const val START_GAME_ENDPOINT = "/game/start/"
         const val USERS_LOBBY_ENDPOINT = "/game/lobby/users/"
-        const val LEAVE_LOBBY_ENDPOINT = "/game/leave/  "
+        const val LEAVE_LOBBY_ENDPOINT = "/game/lobby/leave"
         //char limits
         const val MAX_USERNAME_SiZE = 20
         const val MAX_PASSWORD_SiZE = 20
