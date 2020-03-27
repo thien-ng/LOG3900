6ab88fdce298b44e60309e7c592a691771c9159f
Syn92 <35808535+Syn92@users.noreply.github.com>
2020-03-27 19:09:15 -0400

    D: refresh when construct

--- desktop_app/PolyPaint/VueModeles/GamelistViewModel.cs
+++ desktop_app/PolyPaint/VueModeles/GamelistViewModel.cs
@@ -20,11 +20,10 @@ namespace PolyPaint.VueModeles
     {
         public GamelistViewModel()
         {
-
             _gameCards = new ObservableCollection<GameCard>();
-            getLobbies();
             Numbers = new ObservableCollection<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
             _selectedMode = "FFA";
+            getLobbies();
         }
 
         #region Public Attributes
