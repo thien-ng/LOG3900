d257d69a14cd4b3a5291b7a91894bc706853cce5
Duc-Thien Nguyen <duc-thien@sympatico.ca>
2020-03-29 22:48:00 -0400

    D: update xaml

--- desktop_app/PolyPaint/Modeles/Game.cs
+++ desktop_app/PolyPaint/Modeles/Game.cs
@@ -3,11 +3,13 @@
     class Game
     {
         public string _date { get; set; }
+        public string _mode { get; set; }
         public PlayerScore[] _players;
 
-        public Game(string date, PlayerScore[] players) 
+        public Game(string date, string mode, PlayerScore[] players) 
         {
             _date = date;
+            _mode = mode;
             _players = players;
         }
     }
