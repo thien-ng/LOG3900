28b339d103e90d85408e25b879aabc1ab2ffc62e
Syn92 <35808535+Syn92@users.noreply.github.com>
2020-03-30 16:54:14 -0400

    D: Add gameView + gameViewModel

---
+++ desktop_app/PolyPaint/VueModeles/GameViewModel.cs
@@ -0,0 +1,30 @@
+﻿using PolyPaint.Utilitaires;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolyPaint.VueModeles
+{
+    class GameViewModel: BaseViewModel, IPageViewModel
+    {
+        public GameViewModel()
+        {
+            Console.WriteLine("GameViewModel");
+            DrawViewModel = new DessinViewModel(800,800);
+        }
+
+        #region Public Attributes
+        public DessinViewModel DrawViewModel { get; set; }
+        #endregion
+
+        #region Methods
+
+        #endregion
+
+        #region Commands
+
+        #endregion
+    }
+}
