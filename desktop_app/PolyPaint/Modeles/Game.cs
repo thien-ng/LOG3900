2f13b4cf224d7c4ac8aaeacecd29b51634fde016
Duc-Thien Nguyen <duc-thien@sympatico.ca>
2020-03-30 02:00:55 -0400

    D: add players

--- desktop_app/PolyPaint/Modeles/Game.cs
+++ desktop_app/PolyPaint/Modeles/Game.cs
@@ -4,13 +4,18 @@
     {
         public string _date { get; set; }
         public string _mode { get; set; }
-        public PlayerScore[] _players;
+        //public PlayerScore[] _players;
+        public string _playerScore { get; set; }
 
         public Game(string date, string mode, PlayerScore[] players) 
         {
             _date = date;
             _mode = mode;
-            _players = players;
+            //_players = players;
+            foreach (PlayerScore player in players) 
+            {
+                _playerScore += (player._username + ": " + player._score + ", ");
+            }
         }
     }
 }
