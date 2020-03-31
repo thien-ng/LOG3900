df13bb92c09017390a8741c67420222a7a1270f6
fx46 <31199346+fx46@users.noreply.github.com>
2020-03-31 16:32:19 -0400

    D: New Game chat message items

--- android_app/app/src/main/java/com/example/client_leger/ChatItems.kt
+++ android_app/app/src/main/java/com/example/client_leger/ChatItems.kt
@@ -17,10 +17,31 @@ class ChatItemReceived(val message: String, val username: String, val time: Stri
     }
 }
 
-class ChatItemSent(val text: String, val time: String): Item<ViewHolder>(){
+class GameChatItemReceived(val message: String, val username: String): Item<ViewHolder>(){
+    override fun bind(viewHolder: ViewHolder, position: Int){
+        viewHolder.itemView.their_message_body.text = message
+        viewHolder.itemView.name.text = username
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.their_message_layout
+    }
+}
+
+class ChatItemSent(val text: String, private val time: String): Item<ViewHolder>(){
     override fun bind(viewHolder: ViewHolder, position: Int){
         viewHolder.itemView.my_message_body.text = text
         viewHolder.itemView.my_message_time.text = time
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.my_message_layout
+    }
+}
+
+class GameChatItemSent(val message: String): Item<ViewHolder>(){
+    override fun bind(viewHolder: ViewHolder, position: Int){
+        viewHolder.itemView.their_message_body.text = message
     }
 
     override fun getLayout(): Int {
