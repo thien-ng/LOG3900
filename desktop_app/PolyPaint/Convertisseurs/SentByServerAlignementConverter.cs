bd0737f2c0b5de0ffa50f863fc4cd2c46ff834de
Syn92 <35808535+Syn92@users.noreply.github.com>
2020-03-31 17:40:43 -0400

    D: add converter

---
+++ desktop_app/PolyPaint/Convertisseurs/SentByServerAlignementConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PolyPaint.Convertisseurs
+{
+    class SentByServerAlignementConverter : BaseConverter<SentByMeToAlignementConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? HorizontalAlignment.Center : HorizontalAlignment.Right;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => DependencyProperty.UnsetValue;
+    }
+}
---
+++ desktop_app/PolyPaint/Convertisseurs/SentByServerToColorConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolyPaint.Convertisseurs
+{
+    class SentByServerToColorConverter : BaseConverter<SentByMeToColorConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? "LightGray" : "LightGreen";
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => System.Windows.DependencyProperty.UnsetValue;
+    }
+}
