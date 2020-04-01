cbf5c241cff26416972d4d7a0c1d1e212ab7810e
FX <francois-xavier.legault@polymtl.ca>
2020-03-31 20:37:36 -0400

    R: refactor

--- android_app/app/src/main/java/com/example/client_leger/ChatItems.kt
+++ android_app/app/src/main/java/com/example/client_leger/ChatItems.kt
@@ -18,27 +18,6 @@ class ChatItemReceived(val message: String, val username: String, private val ti
     }
 }
 
-class GameChatItemReceived(val message: String, val username: String): Item<ViewHolder>(){
-    override fun bind(viewHolder: ViewHolder, position: Int){
-        viewHolder.itemView.their_message_body.text = message
-        viewHolder.itemView.name.text = username
-    }
-
-    override fun getLayout(): Int {
-        return R.layout.their_message_layout
-    }
-}
-
-class GameServerChatItemReceived(val message: String): Item<ViewHolder>(){
-    override fun bind(viewHolder: ViewHolder, position: Int){
-        viewHolder.itemView.server_message_body.text = message
-    }
-
-    override fun getLayout(): Int {
-        return R.layout.server_message_layout
-    }
-}
-
 class ChatItemSent(val text: String, private val time: String): Item<ViewHolder>(){
     override fun bind(viewHolder: ViewHolder, position: Int){
         viewHolder.itemView.my_message_body.text = text
@@ -53,9 +32,32 @@ class ChatItemSent(val text: String, private val time: String): Item<ViewHolder>
 class GameChatItemSent(val message: String): Item<ViewHolder>(){
     override fun bind(viewHolder: ViewHolder, position: Int){
         viewHolder.itemView.my_message_body.text = message
+        viewHolder.itemView.my_message_time.text = ""
     }
 
     override fun getLayout(): Int {
         return R.layout.my_message_layout
+    }
+}
+
+class GameChatItemReceived(val message: String, val username: String): Item<ViewHolder>(){
+    override fun bind(viewHolder: ViewHolder, position: Int){
+        viewHolder.itemView.their_message_body.text = message
+        viewHolder.itemView.name.text = username
+        viewHolder.itemView.their_message_time.text = ""
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.their_message_layout
+    }
+}
+
+class GameServerChatItemReceived(val message: String): Item<ViewHolder>(){
+    override fun bind(viewHolder: ViewHolder, position: Int){
+        viewHolder.itemView.server_message_body.text = message
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.server_message_layout
     }
 }
