85ca1561987a9d174434d1091d7a796890c86aa8
fx46 <31199346+fx46@users.noreply.github.com>
2020-04-02 00:06:11 -0400

    D: Handling socket lobby emits

--- android_app/app/src/main/java/com/example/client_leger/SocketIO.kt
+++ android_app/app/src/main/java/com/example/client_leger/SocketIO.kt
@@ -24,6 +24,8 @@ object SocketIO {
         socket.on("game-timer")     { Communication.updateTimer(it[0] as JSONObject) }
         socket.on("game-drawer")    { Communication.updateDrawer(it[0] as JSONObject) }
         socket.on("game-chat")      { Communication.updateGameChat(it[0] as JSONObject) }
+        socket.on("lobby-chat")     { Communication.updateLobbyChat(it[0] as JSONObject) }
+        socket.on("lobby-notif")    { Communication.updateLobby(it[0] as JSONObject) }
     }
 
     fun connect(username: String) {
