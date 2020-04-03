8ed5fbf6cb0964e8edf70606069177f4ff5b3007
Syn92 <Syn92@users.noreply.github.com>
2020-04-02 23:30:54 -0400

    Revert "B: fix self destruct lobby"
    This reverts commit f28a28d743fd0caaef6ffeff487a4e9f4948444c.

--- desktop_app/PolyPaint/VueModeles/LobbyViewModel.cs
+++ desktop_app/PolyPaint/VueModeles/LobbyViewModel.cs
@@ -26,7 +26,6 @@ namespace PolyPaint.VueModeles
             fetchUsername();
             ServerService.instance.socket.On("lobby-notif", data => refreshUserList((JObject)data));
             ServerService.instance.socket.On("game-start", joingame);
-            MyUsername = ServerService.instance.username;
 
             Bots = new ObservableCollection<string> { "bot:sebastien", "bot:olivia", "bot:olivier" };
         }
@@ -42,7 +41,6 @@ namespace PolyPaint.VueModeles
             set { _usernames = value; ProprieteModifiee(); }
         }
 
-
         private ObservableCollection<string> _bots;
         public ObservableCollection<string> Bots
         {
@@ -56,8 +54,6 @@ namespace PolyPaint.VueModeles
             get { return _selectedBot; }
             set { _selectedBot = value; ProprieteModifiee(); }
         }
-
-        public string MyUsername { get; set; }
 
         private bool _isGameMaster;
         public bool IsGameMaster
@@ -136,8 +132,9 @@ namespace PolyPaint.VueModeles
             {
                 fetchUsername();
             }
+            if(!Usernames.Contains(ServerService.instance.username))
+                Mediator.Notify("LeaveLobby", "");
         }
-
         private async void fetchUsername()
         {
             ObservableCollection<string> usernames = new ObservableCollection<string>();
@@ -155,10 +152,8 @@ namespace PolyPaint.VueModeles
                         usernames.Add(item);
                     });
                 }
-                IsGameMaster = ServerService.instance.username == MyUsername;
-                if(IsGameMaster)
-                    usernames.Remove(usernames[0]);
                 Usernames = usernames;
+                IsGameMaster = ServerService.instance.username == Usernames.First<string>();
             }
         }
         private async Task startGame()
