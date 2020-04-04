c09be58f92ae6abc1aa6326cbc2db66d3f189c20
Syn92 <35808535+Syn92@users.noreply.github.com>
2020-04-04 16:36:58 -0400

    D: invite user

--- desktop_app/PolyPaint/VueModeles/LobbyViewModel.cs
+++ desktop_app/PolyPaint/VueModeles/LobbyViewModel.cs
@@ -23,8 +23,11 @@ namespace PolyPaint.VueModeles
         public LobbyViewModel(string lobbyname)
         {
             Usernames = new ObservableCollection<UserLobby>();
+            OnlineUsers = new ObservableCollection<string>();
             this.LobbyName = lobbyname;
+            _searchString = "";
             fetchUsername();
+            getOnlineUsers();
             ServerService.instance.socket.On("lobby-notif", data => refreshUserList((JObject)data));
             ServerService.instance.socket.On("game-start", joingame);
             ServerService.instance.socket.On("lobby-kicked", kickedFromLobby);
@@ -49,6 +52,25 @@ namespace PolyPaint.VueModeles
         {
             get { return _bots; }
             set { _bots = value; ProprieteModifiee(); }
+        }
+
+        private ObservableCollection<string> _onlineUsers;
+        public ObservableCollection<string> OnlineUsers
+        {
+            get { return _onlineUsers; }
+            set { _onlineUsers = value; ProprieteModifiee(); }
+        }
+
+        private string _searchString;
+        public string SearchString
+        {
+            get { return _searchString; }
+            set
+            {
+                _searchString = value;
+                getOnlineUsers();
+                ProprieteModifiee();
+            }
         }
 
         private string _selectedBot;
@@ -89,6 +111,30 @@ namespace PolyPaint.VueModeles
             {
                 if (_dialogContent == value) return;
                 _dialogContent = value;
+                ProprieteModifiee();
+            }
+        }
+
+        private bool _isInviteUserDialogOpen;
+        public bool IsInviteUserDialogOpen
+        {
+            get { return _isInviteUserDialogOpen; }
+            set
+            {
+                if (_isInviteUserDialogOpen == value) return;
+                _isInviteUserDialogOpen = value;
+                ProprieteModifiee();
+            }
+        }
+
+        private object _inviteDialogContent;
+        public object InviteDialogContent
+        {
+            get { return _inviteDialogContent; }
+            set
+            {
+                if (_inviteDialogContent == value) return;
+                _inviteDialogContent = value;
                 ProprieteModifiee();
             }
         }
@@ -155,6 +201,7 @@ namespace PolyPaint.VueModeles
         }
         private async void fetchUsername()
         {
+            Usernames.Clear();
             ObservableCollection<UserLobby> usernames = new ObservableCollection<UserLobby>();
             var response = await ServerService.instance.client.GetAsync(Constants.SERVER_PATH + Constants.USERS_LOBBY_PATH + LobbyName);
             if (response.IsSuccessStatusCode)
@@ -184,6 +231,29 @@ namespace PolyPaint.VueModeles
                 temp.Remove(temp.First<UserLobby>());
             }
             return temp.First<UserLobby>().username;
+        }
+
+        private async void getOnlineUsers()
+        {
+            OnlineUsers.Clear();
+            ObservableCollection<string> usernames = new ObservableCollection<string>();
+            var response = await ServerService.instance.client.GetAsync(Constants.SERVER_PATH + Constants.ONLINE_USERS_PATH + SearchString);
+            if (response.IsSuccessStatusCode)
+            {
+                StreamReader streamReader = new StreamReader(await response.Content.ReadAsStreamAsync());
+                String responseData = streamReader.ReadToEnd();
+
+                var myData = JsonConvert.DeserializeObject<List<String>>(responseData);
+                foreach (var item in myData)
+                {
+                    App.Current.Dispatcher.Invoke(delegate
+                    {
+                        if(item!=ServerService.instance.username)
+                            usernames.Add(item);
+                    });
+                }
+                OnlineUsers = usernames;
+            }
         }
 
         private async Task startGame()
@@ -217,6 +287,20 @@ namespace PolyPaint.VueModeles
             if(response.IsSuccessStatusCode)
                 Bots.Remove(SelectedBot);
         }
+        private async Task InviteUserAsync(string x)
+        {
+            string requestPath = Constants.SERVER_PATH + Constants.GAME_INVITE_PATH;
+            dynamic values = new JObject();
+            values.username = x;
+            values.lobbyName = this.LobbyName;
+            var content = JsonConvert.SerializeObject(values);
+            var buffer = System.Text.Encoding.UTF8.GetBytes(content);
+            var byteContent = new ByteArrayContent(buffer);
+            byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            var response = await ServerService.instance.client.PostAsync(requestPath, byteContent);
+            if (response.IsSuccessStatusCode)
+                MessageBox.Show("Invite sucessfully sent");
+        }
 
         #endregion
 
@@ -242,6 +326,19 @@ namespace PolyPaint.VueModeles
                 return _leaveLobbyCommand ?? (_leaveLobbyCommand = new RelayCommand(async x =>
                 {
                     await Task.Run(() => leaveLobby());
+                }));
+            }
+        }
+        
+        private ICommand _openInviteUserControl;
+        public ICommand OpenInviteUserControl
+        {
+            get
+            {
+                return _openInviteUserControl ?? (_openInviteUserControl = new RelayCommand(async x =>
+                {
+                    InviteDialogContent = new InviteUserControl();
+                    IsInviteUserDialogOpen = true;
                 }));
             }
         }
@@ -285,6 +382,22 @@ namespace PolyPaint.VueModeles
                 }));
             }
         }
+
+        private ICommand _inviteUserCommand;
+        public ICommand InviteUserCommand
+        {
+            get
+            {
+                return _inviteUserCommand ?? (_inviteUserCommand = new RelayCommand(async x =>
+                {
+                    Console.WriteLine("invite user: " + (string)x);
+                    await Task.Run(() => InviteUserAsync((string)x));
+                    IsInviteUserDialogOpen = false;
+                }));
+            }
+        }
+
+
         private ICommand _removeUserCommand;
         public ICommand RemoveUserCommand
         {
