264afcf7f9b5309b4524a43339d5605503cecde6
fx46 <31199346+fx46@users.noreply.github.com>
2020-04-08 00:44:43 -0400

    D: end screen WIP

--- android_app/app/src/main/java/com/example/client_leger/Fragments/GameplayFragment.kt
+++ android_app/app/src/main/java/com/example/client_leger/Fragments/GameplayFragment.kt
@@ -3,13 +3,17 @@ package com.example.client_leger.Fragments
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.PopupWindow
 import com.example.client_leger.Communication.Communication
 import com.example.client_leger.Interface.FragmentChangeListener
 import com.example.client_leger.R
+import com.example.client_leger.RankItem
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.ViewHolder
 import io.reactivex.rxjava3.disposables.Disposable
+import kotlinx.android.synthetic.main.fragment_chat.view.*
+import kotlinx.android.synthetic.main.popup_end_game.view.*
 
 class GameplayFragment: Fragment(), FragmentChangeListener {
 
@@ -24,9 +28,41 @@ class GameplayFragment: Fragment(), FragmentChangeListener {
             .commit()
 
         endGameSub = Communication.getEndGameListener().subscribe{ res ->
-            // TODO show points from res, and then change to lobbyCardsFragment
-            Log.w("Points", res.toString())
-            replaceFragment(LobbyCardsFragment())
+            activity!!.runOnUiThread {
+                val rankingView = layoutInflater.inflate(R.layout.popup_end_game, null)
+                val popupWindow = PopupWindow(
+                    rankingView,
+                    WindowManager.LayoutParams.WRAP_CONTENT,
+                    WindowManager.LayoutParams.WRAP_CONTENT,
+                    false
+                )
+
+                val rankAdapter = GroupAdapter<ViewHolder>()
+                rankingView.recyclerView_ranks.adapter = rankAdapter
+
+                val ranks = res.getJSONArray("points")
+                Log.w("rank", "ranks.length(): " + ranks.length())
+
+                for (i in 0 until ranks.length()) {
+                    Log.w("rank", i.toString())
+                    val infos = ranks.getJSONObject(i)
+                    Log.w("rank", "ranks.getJSONObject(i): " + ranks.getJSONObject(i).toString())
+                    Log.w("rank", "infos.getString(\"username\"): " + infos.getString("username"))
+                    Log.w("rank", "infos.getInt(\"points\"): " + infos.getInt("points"))
+                    rankAdapter.add(RankItem(
+                        i + 1,
+                        infos.getString("username"),
+                        infos.getInt("points")
+                    ))
+                }
+
+                popupWindow.showAtLocation(rankingView, Gravity.CENTER, 0, 0)
+
+                rankingView.button_ok.setOnClickListener {
+                    replaceFragment(LobbyCardsFragment())
+                    popupWindow.dismiss()
+                }
+            }
         }
 
         return v
@@ -47,5 +83,4 @@ class GameplayFragment: Fragment(), FragmentChangeListener {
         super.onDestroy()
         endGameSub.dispose()
     }
-
 }
