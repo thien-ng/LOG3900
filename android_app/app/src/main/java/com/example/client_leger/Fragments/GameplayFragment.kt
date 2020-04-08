866b019d47c6622964443b047eae40cf9c54b5fb
fx46 <31199346+fx46@users.noreply.github.com>
2020-04-08 03:02:03 -0400

    D: New table display for rankings

--- android_app/app/src/main/java/com/example/client_leger/Fragments/GameplayFragment.kt
+++ android_app/app/src/main/java/com/example/client_leger/Fragments/GameplayFragment.kt
@@ -1,10 +1,13 @@
 package com.example.client_leger.Fragments
 
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.util.Log
 import android.view.*
 import android.widget.PopupWindow
+import android.widget.TableRow
+import android.widget.TextView
 import com.example.client_leger.Communication.Communication
 import com.example.client_leger.Interface.FragmentChangeListener
 import com.example.client_leger.R
@@ -14,6 +17,8 @@ import com.xwray.groupie.ViewHolder
 import io.reactivex.rxjava3.disposables.Disposable
 import kotlinx.android.synthetic.main.fragment_chat.view.*
 import kotlinx.android.synthetic.main.popup_end_game.view.*
+import java.util.*
+
 
 class GameplayFragment: Fragment(), FragmentChangeListener {
 
@@ -37,8 +42,30 @@ class GameplayFragment: Fragment(), FragmentChangeListener {
                     false
                 )
 
-                val rankAdapter = GroupAdapter<ViewHolder>()
-                rankingView.recyclerView_ranks.adapter = rankAdapter
+                var tableRow = TableRow(this.context)
+                tableRow.layoutParams = TableRow.LayoutParams(
+                    TableRow.LayoutParams.MATCH_PARENT,
+                    TableRow.LayoutParams.WRAP_CONTENT
+                )
+
+                val title1 = TextView(this.context)
+                title1.text = "Rank"
+                title1.textSize = 20F
+                tableRow.addView(title1)
+
+                val title2 = TextView(this.context)
+                title2.setPadding(120 - title1.width, 0, 0, 0)
+                title2.textSize = 20F
+                title2.text = "Username"
+                tableRow.addView(title2)
+
+                val title3 = TextView(this.context)
+                title3.setPadding(110, 0, 0, 0)
+                title3.text = "Points"
+                title3.textSize = 20F
+                tableRow.addView(title3)
+
+                rankingView.tableView_ranks.addView(tableRow)
 
                 val ranks = res.getJSONArray("points")
                 Log.w("rank", "ranks.length(): " + ranks.length())
