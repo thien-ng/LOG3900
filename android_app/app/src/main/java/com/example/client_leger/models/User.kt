3ac5e27534c6bbfaa84d1ff364dc7c8e42ce26bd
PChausseC <chaussepablo@gmail.com>
2020-04-09 16:09:30 -0400

    D: match history on profile

--- android_app/app/src/main/java/com/example/client_leger/models/User.kt
+++ android_app/app/src/main/java/com/example/client_leger/models/User.kt
@@ -1,11 +1,26 @@
 package com.example.client_leger.models
 
 import org.json.JSONArray
+import org.json.JSONObject
 
-class User(username:String, fName:String, lName:String, connections: JSONArray) {
+class User(username:String, fName:String, lName:String, connections: JSONArray, stats:JSONObject, games: JSONArray) {
     var name = username
     var fName = fName
     var lName = lName
 
     var connections = connections
+
+    var stats:Stats = Stats(stats.getInt("totalGame"), stats.getDouble("winRate"), stats.getInt("bestScore"), stats.getInt("totalPlayTime") , stats.getDouble("avgGameTime"))
+    var games:ArrayList<Game> = jsonArrayToGameArray(games)
+
+    private fun jsonArrayToGameArray(games: JSONArray): ArrayList<Game> {
+        var tempArray: ArrayList<Game> = arrayListOf()
+        for (i in 0 until games.length()) {
+            var jsonGame = games.getJSONObject(i)
+            var tempGame = Game(jsonGame.getString("mode"),jsonGame.getString("date"), jsonGame.getJSONArray("players").toString())
+            tempArray.add(tempGame)
+        }
+        return tempArray
+    }
+
 }
