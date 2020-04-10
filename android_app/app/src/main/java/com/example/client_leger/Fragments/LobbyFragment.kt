1b1dc92bd309c12da1b8d4799ea590e0e50aa53e
fx46 <31199346+fx46@users.noreply.github.com>
2020-04-10 02:42:33 -0400

    R: removed import

--- android_app/app/src/main/java/com/example/client_leger/Fragments/LobbyFragment.kt
+++ android_app/app/src/main/java/com/example/client_leger/Fragments/LobbyFragment.kt
@@ -3,7 +3,6 @@ package com.example.client_leger.Fragments
 import android.app.AlertDialog
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
